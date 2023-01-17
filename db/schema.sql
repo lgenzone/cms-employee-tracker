2d32ed77ec6c17e5e7d92959dfcfe5f0c5e6ceef
lgenzone <lgenzone@icloud.com>
2023-01-16 17:00:04 -0800

    create department, role, employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,32 @@
-DROP DATABASE IF EXISTS user_db;
+-- if this database already exists, delete it --
+DROP DATABASE IF EXISTS business_db; 
+-- create new database titled 'business'
+CREATE DATABASE business_db;
+-- select created tab;e --
+USE business_db;
 
-CREATE DATABASE user_db;
+-- create new table for 'department'
+CREATE TABLE department (
+    -- give unique id and set primary key --
+    id INT PRIMARY KEY,
+    -- hold department name (max 30 characters) --
+    name VARCHAR (30)
+);
+ 
+ -- create new table for 'role'
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL
+);
+
+-- create new table for 'employee'
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    -- null if the employee has no manager --
+    manager_id: INT
+);
