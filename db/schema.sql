59d040578e9c1cc4a9b94b262383bf7ba46de63c
lgenzone <lgenzone@icloud.com>
2023-02-06 23:15:41 -0700

    finish tables and add to choices

--- db/schema.sql
+++ db/schema.sql
@@ -4,20 +4,32 @@ CREATE DATABASE business_db;
 USE business_db;
 
 CREATE TABLE department (
-    id INT NOT NULL, 
+    id INT NOT NULL AUTO_INCREMENT, 
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
  
  -- create new table for 'role' --
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
+    PRIMARY KEY (department_id)
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 -- create new table for 'employee' -- 
+create TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT, 
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ function init () {
       type: 'list',
       message: 'What would you like to do?',
       name: 'menuChoice',
-      choices: ['View all departments', 'View all roles', 'View all employees'],
+      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update employee role' ],
     },
   ])
   .then((data) => {
